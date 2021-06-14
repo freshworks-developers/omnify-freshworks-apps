c32ee81d6aa46caf597436d13e40165b86f29e14
Saif Ali Shaik <saifshine7@gmail.com>
2021-06-14 10:44:08 +0530

    🚀 RELEASE: dynamic iparam validation

--- config/assets/iparams.js
+++ config/assets/iparams.js
@@ -9,12 +9,10 @@ function renderConfigPage(currentProduct) {
 
   switch (product) {
     case 'freshsales':
-      console.log('freshsales');
       utils.set('api_key', personalizeAPIField('Freshsales'));
       utils.set('freshworks_crm_field', { visible: false });
       break;
     case 'freshworks_crm':
-      console.log('freshworks crm');
       utils.set('api_key', personalizeAPIField('Freshworks CRM'));
       utils.set('freshsales_field', { visible: false });
       break;
@@ -31,28 +29,27 @@ function renderConfigPage(currentProduct) {
 async function establishAPIConnection(api_key) {
   try {
     let { name: productName, url: host_url } = client.context.productContext;
-    console.log(productName, host_url, api_key);
     let options = {
-      Authorization: `Token token=${api_key}`,
-      'Content-Type': 'application/json'
+      headers: {
+        Authorization: `Token token=${api_key}`,
+        'Content-Type': 'application/json'
+      }
     };
     let URL = genHostEndpoint(productName, host_url);
-    console.log('url', URL);
-    client.request.get(URL, options).then(
-      function (data) {
-        console.log(`On Autheticating: ${data}`);
-      },
-      function (error) {
-        console.log(error);
-      }
-    );
+    let res = await client.request.get(URL, options);
+    if (res.status == '200') return '';
   } catch (error) {
     console.error(error);
-    return `4XX NOT OK`;
+    return `Invalid API Key`;
   }
 
   function genHostEndpoint(productName, host_url) {
-    return `${host_url}/sales/api/contacts/16001771741`;
+    switch (productName) {
+      case 'freshsales':
+        return `${host_url}/api/settings/deals/fields`;
+      case 'freshworks_crm':
+        return `${host_url}/api/sales_activites`;
+    }
   }
 }
 
